589e25259dc82341affdd57acc1a35393c6d37a9
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-11-10 18:18:58 -0500

    Implement mailers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -53,6 +53,7 @@ class OrdersController < ApplicationController
       )
     end
     order.save!
+    User.confirmation_email(order).deliver_now
     order
   end
 
--- app/mailers/user.rb
+++ app/mailers/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationMailer
+  default from: "no-reply@jungle.com"
+
+  def confirmation_email(order)
+    @order = order
+    @order_items = order.line_items
+    mail(to: @order.email, subject: "Order #" + @order.id.to_s + " placed!")
+  end
+  
 end
